44203f38d89e84a9a94f80ee2f567c7a9c923439
blackspherefollower <darktech@black-sphere.co.uk>
2023-12-19 10:38:00 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿
+// Create and initialize a new trigger to configure it.
+using Windows.Devices.Bluetooth;
+using Windows.Devices.Bluetooth.Advertisement;
+using Windows.Storage.Streams;
+
+BluetoothLEAdvertisementPublisher publisher = new BluetoothLEAdvertisementPublisher();
+
+// We need to add some payload to the advertisement. A publisher without any payload
+// or with invalid ones cannot be started. We only need to configure the payload once
+// for any publisher.
+
+// Add a manufacturer-specific section:
+// First, create a manufacturer data section
+var manufacturerData = new BluetoothLEManufacturerData();
+
+// Then, set the company ID for the manufacturer data. Here we picked an unused value: 0xFFFE
+manufacturerData.CompanyId = 0xFFF0;
+
+// Finally set the data payload within the manufacturer-specific section
+// Here, use a 16-bit UUID: 0x1234 -> {0x34, 0x12} (little-endian)
+var writer = new DataWriter();
+writer.WriteByte(0x6D);
+writer.WriteByte(0xB6);
+writer.WriteByte(0x43);
+writer.WriteByte(0xCE);
+writer.WriteByte(0x97);
+writer.WriteByte(0xFE);
+writer.WriteByte(0x42);
+writer.WriteByte(0x7C);
+// dynamic bit
+writer.WriteByte(0xF5);
+writer.WriteByte(0x94);
+writer.WriteByte(0x6D);
+
+// Make sure that the buffer length can fit within an advertisement payload. Otherwise you will get an exception.
+manufacturerData.Data = writer.DetachBuffer();
+
+// Add the manufacturer data to the advertisement publisher:
+publisher.Advertisement.ManufacturerData.Add(manufacturerData);
+
+// Extended advertisements are BT5 only stuff: doesn't work with CSR4s
+//publisher.UseExtendedAdvertisement = true;
+
+// Set the required flags (not entirely sure how these flag map to what we need)
+// Disabled as this will cause a runtime exception
+//publisher.Advertisement.Flags = BluetoothLEAdvertisementFlags.GeneralDiscoverableMode;
+
+publisher.Start();
+
+// Display the current status of the publisher
+Console.WriteLine($"Published Status: {publisher.Status}, Error: {BluetoothError.Success}");
+
+Thread.Sleep(60000);
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # GattAdvertisementTest
+
+This is a quick test to see if the Bluetooth Advertisement features exposed by the Windows SDK can be used to control MuSe/LoveSpouce/etc devices
+
+## Conclusion
+
+Nope.
+
+Whilst it is possible to craft and publish the manufacturer data required, the SDK prevents you from changing the flags required to get the devices to read the advertisement as a command.
+
+This is explicitly called out in https://learn.microsoft.com/en-us/uwp/api/windows.devices.bluetooth.advertisement.bluetoothleadvertisementpublisher.advertisement?view=winrt-22621#windows-devices-bluetooth-advertisement-bluetoothleadvertisementpublisher-advertisement:
+```
+When configuring the publisher object, you can't add restricted section types (BluetoothLEAdvertisementPublisher.Advertisement.Flags and BluetoothLEAdvertisementPublisher.Advertisement.LocalName). Trying to set those property values results in a runtime exception. You can still set the manufacturer data section, or any other sections not defined by the list of restrictions.
+```
